2151141465ba845c9efc5c98995af78a2fe7ca61
Umesh Patel <umesh.code1@gmail.com>
2025-01-04 22:13:32 +0530

    add program to find the maximum number in a list

---
+++ Program4/program4.py
@@ -0,0 +1,8 @@
+# Write a python program to find the maximum from a list of numbers.
+
+
+list=[n]
+n=int(input("Enter the number of elements in the list: "))
+for i in range(n):
+    list.append(int(input(f"Enter element {i+1}: "))
+print(f"The maximum number in the list is {max(list)}")
